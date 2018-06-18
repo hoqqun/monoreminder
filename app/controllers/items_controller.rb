6bf6e36cf85253eb0ceb67d981f8c0a14054ad8d
sierra <sierra@usernoMacBook-Pro.local>
2018-06-18 23:12:32 +0900

    レイアウト変更

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,37 +2,28 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_item, only: [:show, :edit, :update, :destroy]
 
-  # GET /items
-  # GET /items.json
   def index
     @q = Item.where(user_id: current_user.id).ransack(params[:q])
     @q.sorts = 'id desc'
     @items = @q.result.page(params[:page]).per(20)
   end
 
-  # GET /items/1
-  # GET /items/1.json
   def show
   end
 
-  # GET /items/new
   def new
     @item = Item.new
   end
 
-  # GET /items/1/edit
   def edit
   end
 
-  # POST /items
-  # POST /items.json
   def create
     @item = User.find(current_user.id).items.build(item_params)
-    #@item = Item.new(item_params)
 
     respond_to do |format|
       if @item.save
-        format.html { redirect_to @item, notice: 'Item was successfully created.' }
+        format.html { redirect_to @item, notice: t('flash.items.create') }
         format.json { render :show, status: :created, location: @item }
       else
         format.html { render :new }
@@ -41,12 +32,10 @@ class ItemsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /items/1
-  # PATCH/PUT /items/1.json
   def update
     respond_to do |format|
       if @item.update(item_params)
-        format.html { redirect_to @item, notice: 'Item was successfully updated.' }
+        format.html { redirect_to @item, notice: t('flash.items.update') }
         format.json { render :show, status: :ok, location: @item }
       else
         format.html { render :edit }
@@ -55,23 +44,19 @@ class ItemsController < ApplicationController
     end
   end
 
-  # DELETE /items/1
-  # DELETE /items/1.json
   def destroy
     @item.destroy
     respond_to do |format|
-      format.html { redirect_to items_url, notice: 'Item was successfully destroyed.' }
+      format.html { redirect_to items_url, notice: t('flash.items.destroy') }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_item
       @item = Item.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def item_params
       params.require(:item).permit(:name, :price, :purchase_date, :memo, :image1, :image2, :image3, :location_id, :category_id)
     end
