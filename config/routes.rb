f90eed51c930a863e416a98070fdeabeb812db7c
sierra <sierra@usernoMacBook-Pro.local>
2018-06-26 23:10:29 +0900

    bug サインアップ時のメソッドを訂正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   #devise_for :views
-  root to: "homes#index"
   devise_for :users
+  root to: "homes#index"
   get 'homes/index'
   resources :locations
   resources :categories
